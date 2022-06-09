8acd91f9a40d433b6028d27e9e15a0ab802cffd3
slyman <suleyman.aktas@gmail.com>
2022-06-09 17:40:33 +0300

    user obj was refactored

--- db_operations/connection/oracle/credentials/user.py
+++ db_operations/connection/oracle/credentials/user.py
@@ -2,18 +2,18 @@ from strenum import StrEnum
 
 
 class QaUser(StrEnum):
-    USERNAME = "sch_dmall"
-    PASSWORD = "sch_dmall123",
-    DSN = "QA_N11"
+    USERNAME = "****"
+    PASSWORD = "****",
+    DSN = "****"
 
 
 class TestUser(StrEnum):
-    USERNAME = "sch_dmall"
-    PASSWORD = "dmalltest",
-    DSN = "TEST_N11"
+    USERNAME = "****"
+    PASSWORD = "****",
+    DSN = "****"
 
 
 class StUser(StrEnum):
-    USERNAME = "USR_DMALL_ATM_TEST"
-    PASSWORD = "qwzA123*",
-    DSN = "ST_N11"
+    USERNAME = "****"
+    PASSWORD = "*****",
+    DSN = "****"
